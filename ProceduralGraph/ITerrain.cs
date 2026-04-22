879b924bf10a81125df443ce51621ae60cc620be
will11600 <wb11600@gmail.com>
2026-04-22 17:06:49 +0100

    Add scene and terrain interfaces to ProceduralGraph
    Introduced ISceneMember for scene graph hierarchy, ITerrain for terrain objects with async rebuild support, and ITerrainProvider as a factory interface. These interfaces provide a foundation for extensible scene management and terrain manipulation.

---
+++ ProceduralGraph/ITerrain.cs
@@ -0,0 +1,30 @@
+﻿using ProceduralGraph.Collections.Unsafe;
+using ProceduralGraph.Mathematics;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProceduralGraph;
+
+/// <summary>
+/// Defines the contract for a terrain object within a scene.
+/// </summary>
+public interface ITerrain : ISceneMember
+{
+    /// <summary>
+    /// Gets the dimensions of the terrain.
+    /// </summary>
+    Int2 Size { get; }
+
+    /// <summary>
+    /// Asynchronously rebuilds the terrain data using the specified size, height map, and splat weight maps.
+    /// </summary>
+    /// <param name="heights">A writable map containing the height values for each terrain cell.</param>
+    /// <param name="splatWeights">
+    /// A read-only memory of splat weight maps, where each map represents the blending weights for a terrain texture
+    /// layer. The number of maps determines the number of texture layers.
+    /// </param>
+    /// <param name="cancellationToken">A token that can be used to cancel the asynchronous operation.</param>
+    /// <returns>A <see cref="ValueTask"/> that represents the asynchronous rebuild operation.</returns>
+    ValueTask RebuildAsync(UnmanagedMap<float> heights, ReadOnlyMemory<UnmanagedMap<Pixel32>> splatWeights, CancellationToken cancellationToken);
+}
---
+++ ProceduralGraph/ITerrainProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace ProceduralGraph;
+
+/// <summary>
+/// Defines a contract for creating terrain instances.
+/// </summary>
+public interface ITerrainProvider
+{
+    /// <summary>
+    /// Creates a new terrain instance.
+    /// </summary>
+    /// <returns>The created terrain instance.</returns>
+    public ITerrain CreateInstance();
+}
